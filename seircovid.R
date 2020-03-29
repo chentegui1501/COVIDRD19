45564258adf10c927ea875e16b5831a36e19305c
fardl <fard0783@gmail.com>
2020-03-29 20:05:01 +0000

    Programa seircovid_rd y datos por provincias

--- seircovid.R
+++ seircovid.R
@@ -1,7 +1,7 @@
 ########## Codigo: Modelo SEIR COVID19: Republica Dominicana, con Analisis de Politicas de Movibildad######
 #########  Francisco A. Ramírez / Vicente Peña Peralta
 #########  Universidad Autónoma de Santo Domingo
-
+rm(list=ls())
 #Librerias a utilizar
 library(deSolve)
 library(ggplot2)
@@ -40,7 +40,9 @@ modelo_seir = function (valores_iniciales, valores_estado, parametros)
 #Parametros Calibrados
 period_laten = 14            # periodo latente
 period_incu = 7             # periodo incubacion del virus 
-beta_value =.92    #0.905
+beta_value =.856    #0.905
+beta_value_low = beta_value -0.05
+beta_value_high = beta_value +0.05
 gamma_value = 1 / period_laten
 delta_value = 1 / period_incu
 R0 = beta_value / gamma_value   #R0 Implicito calibración inicial
@@ -49,6 +51,8 @@ kappa0 = 100
 
 
 parametros = c (alpha0 = alpha0, beta0 = beta_value, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
+parametros_low = c (alpha0 = alpha0, beta0 = beta_value_low, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
+parametros_high = c (alpha0 = alpha0, beta0 = beta_value_high, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
 
 #Valores iniciales 
 
@@ -66,27 +70,41 @@ tiempo_disc = seq (0, 300, by=1)
 
 #Solucionando el Sistema 
 output = ode(valores_ini, tiempo_disc, modelo_seir, parametros)
+output_low = ode(valores_ini, tiempo_disc, modelo_seir, parametros_low)
+output_high = ode(valores_ini, tiempo_disc, modelo_seir, parametros_high)
+
+
 
 #cambiar out a un data.frame
 output <- as.data.frame(output*N) #aqui puede multiplicar 'out' por N
+output_low <- as.data.frame(output_low*N) #aqui puede multiplicar 'out' por N
+output_high <- as.data.frame(output_high*N) #aqui puede multiplicar 'out' por N
+
 #eliminar la variable 'time' en out
 output$time <- NULL
+output_low$time <- NULL
+output_high$time <- NULL
+
 
 
 
 #Gráfica de la Simulación
-matplot(x = tiempo_disc, y = output, type = "l",
+matplot(x = tiempo_disc, y = output[,1:4], type = "l",
         xlab = "Tiempo", ylab = "S, E, I, R", main = "Modelo SEIR",
         lwd = 1, lty = 1, bty = "l", col = 2:4)
 #añadir leyenda 
-legend(40, 0.7, c("Susceptibles", "Expuestos", "Infectados", "Recuperados"), 
+legend(100, 0.7, c("Susceptibles", "Expuestos", "Infectados", "Recuperados"), 
        pch = 1, col = 2:4, bty = "n", cex = 1)
 
 
 #Ajuste de los Casos Publicados por Salud Publica
 
-simul <- ts(output[1:27,3],frequency = 365, start = c(2020,62))
+simul <- ts(output[1:29,3],frequency = 365, start = c(2020,62))
+simul_low <- ts(output_low[1:29,3], freq= 365, start =c(2020,62))
+simul_high <- ts(output_high[1:29,3], freq= 365, start =c(2020,62))
 data  <- ts(read_excel("datos_salud_publica.xlsx"),freq =365, start = c(2020,62))
+
+cbind(data, simul)
 
 autoplot(cbind(data,simul),
          xlab = "Dias",
@@ -96,6 +114,21 @@ autoplot(cbind(data,simul),
   labs(caption = "Datos de los Boletines de Salud Pública")+
   scale_color_manual( name = element_blank(), label = c("Datos", "Modelo"), values = c("blue", "red"))+
   theme_bw()
+
+h <- autoplot(simul,
+         xlab = "Dias",
+         ylab = "Infectados")+
+  geom_ribbon(aes(ymin = simul_low, ymax = simul_high), fill = "red",alpha=0.3)+
+  ggtitle(label = "Evolución de los Casos de Infectados",subtitle = "Desempeño del Modelo")+
+  geom_line()
+
+h +
+  geom_point(aes(y=data),col ="blue")+
+  labs(caption = "Datos de los Boletines de Salud Pública")+
+  theme_bw()+
+  scale_color_manual( "", label = c("Datos", "Modelo"), values = c("blue", "red"))
+
+
 
 
 
@@ -135,7 +168,7 @@ output4 = ode(valores_ini, tiempo_disc, modelo_seir, parametros)
 
 #Grafico 15 dias
 #data <- ts(data, freq = 365, start = c(2020, 62), end = c(2020, 102))
-startna <- ts(rep(NA,26), start = c(2020,62))
+startna <- ts(rep(NA,29), start = c(2020,62))
 esc1 <- ts(c(data,output1[1:15,4]*N),freq = 365, start = c(2020,62))
 esc2 <- ts(c(startna,output2[1:15,4]*N),freq = 365, start = c(2020,62))
 esc3 <- ts(c(startna,output3[1:15,4]*N),freq = 365, start = c(2020,62))
@@ -207,5 +240,5 @@ autoplot(cbind(simulc),
   theme_bw()+
   theme(legend.position = "top")
 
-
+cbind(data,simul)
 
---
+++ seircovid_prov.R
@@ -0,0 +1,181 @@
+########## Codigo: Modelo SEIR COVID19: Republica Dominicana, con Analisis de Politicas de Movibildad######
+#########  Francisco A. Ramírez / Vicente Peña Peralta
+#########  Universidad Autónoma de Santo Domingo
+rm(list=ls())
+#Librerias a utilizar
+library(deSolve)
+library(ggplot2)
+library(tseries)
+library(forecast)
+library(readxl)
+
+d    <- ts(read_excel("datos_sp_provincias.xlsx"),freq =365, start = c(2020,62))
+
+
+#Modelo SEIR
+
+modelo_seir = function (valores_iniciales, valores_estado, parametros)
+{
+  # creamos las variables de estados
+  S = valores_estado [1]        # susceptibles
+  E = valores_estado [2]        # expuestos
+  I = valores_estado [3]        # infectados
+  R = valores_estado [4]        # recuperados
+  
+  with ( 
+    as.list (parametros), 
+    {
+      beta = beta0*(1-alpha0)*(1-0.05*I)^kappa0
+      # derivadas parciales
+      dS = (-beta * S * I)
+      dE = (beta * S * I) - (delta * E)
+      dI = (delta * E) - (gamma * I)
+      dR = (gamma * I)
+      
+      tasa_contag = beta/gamma
+      # resultados 
+      results = c (dS, dE, dI, dR)
+      list (results,tasa_contag)
+    }
+  )
+}
+
+
+#Parametros Calibrados Generales
+period_laten = 14            # periodo latente
+period_incu = 7             # periodo incubacion del virus 
+gamma_value = 1 / period_laten
+delta_value = 1 / period_incu
+R0 = beta_value / gamma_value   #R0 Implicito calibración inicial
+alpha0 = 0
+kappa0 = 100
+tiempo_disc = seq (0, 300, by=1)
+
+###----ANALISIS POR PROVINCIAS -----
+
+# Distrito Nacional
+  beta_dn <-  .72
+  p_dn = c (alpha0 = alpha0, beta0 = beta_dn, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
+  #p_dn = c (beta = beta_dn, gamma = gamma_value)
+  W_dn = 965040 -1  # suceptibles_0
+  X_dn = 0           # Expuestos_0
+  Y_dn = 1        # Infectados_0
+  Z_dn = 0           # Recuperados_0
+  N_dn = W_dn + X_dn + Y_dn + Z_dn #Poblacion
+
+  valores_ini = c (S = W_dn/N_dn, E = X_dn/N_dn, I = Y_dn/N_dn, R = Z_dn/N_dn)
+  
+  #Solucionando el Sistema 
+  o_dn = ode(valores_ini, tiempo_disc, modelo_seir, p_dn)
+  o_dn <- as.data.frame(o_dn*N_dn) 
+
+  #Ajuste de los Casos Publicados por Salud Publica
+  s_dn <- ts(o_dn[1:28,3],freq = 365, start = c(2020,62))
+  d_dn <- d[,2]
+
+  data_dn <-  window(cbind(d_dn,s_dn),start=c(2020,80))
+
+  data_dn %>% autoplot(xlab = "Dias",
+           ylab = "Infectados")+
+    ggtitle(label = "Evolución de los Casos de Infectados",
+            subtitle = "Desempeño del Modelo, Distrito Nacional")+
+    labs(caption = "Datos de los Boletines de Salud Pública")+
+    scale_color_manual( name = element_blank(), label = c("Datos", "Modelo"), values = c("blue", "red"))+
+    theme_bw()+
+    geom_point()
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Santo Domingo
+  beta_sd <-  0.535
+  p_sd = c (alpha0 = alpha0, beta0 = beta_sd, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
+  W_sd = 2374370 -1  # suceptibles_0
+  X_sd = 0           # Expuestos_0
+  Y_sd = 1        # Infectados_0
+  Z_sd = 0           # Recuperados_0
+  N_sd = W_sd + X_sd + Y_sd + Z_sd #Poblacion
+  
+  valores_ini = c (S = W_sd/N_sd, E = X_sd/N_sd, I = Y_sd/N_sd, R = Z_sd/N_sd)
+  
+  #Solucionando el Sistema 
+  o_sd = ode(valores_ini, tiempo_disc, modelo_seir, p_sd)
+  o_sd <- as.data.frame(o_sd*N_sd) 
+  
+  #Ajuste de los Casos Publicados por Salud Publica
+  s_sd <- ts(o_sd[1:28,3],freq = 365, start = c(2020,62))
+  d_sd <- d[,33]
+  
+  data_sd <- window(cbind(d_sd,s_sd),start=c(2020,80))
+  
+  data_sd %>% autoplot(xlab = "Dias",
+                       ylab = "Infectados")+
+    ggtitle(label = "Evolución de los Casos de Infectados",
+            subtitle = "Desempeño del Modelo, Santo Domingo")+
+    labs(caption = "Datos de los Boletines de Salud Pública")+
+    scale_color_manual( name = element_blank(), label = c("Datos", "Modelo"), values = c("blue", "red"))+
+    theme_bw()+
+    geom_point()
+  
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Duarte
+  beta_du <-  0.8
+  p_du = c (alpha0 = alpha0, beta0 = beta_du, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
+  W_du = 289574 -0.1  # suceptibles_0
+  X_du = 0           # Expuestos_0
+  Y_du = 0.1         # Infectados_0
+  Z_du = 0           # Recuperados_0
+  N_du = W_du + X_du + Y_du + Z_du #Poblacion
+  
+  valores_ini = c (S = W_du/N_du, E = X_du/N_du, I = Y_du/N_du, R = Z_du/N_du)
+  
+  #Solucionando el Sistema 
+  o_du = ode(valores_ini, tiempo_disc, modelo_seir, p_du)
+  o_du <- as.data.frame(o_du*N_du) 
+  
+  #Ajuste de los Casos Publicados por Salud Publica
+  s_du <- ts(o_du[1:28,3],freq = 365, start = c(2020,62))
+  d_du <- d[,7]
+  
+  data_du <- window(cbind(d_du,s_du),start=c(2020,80))
+  
+  data_du %>% autoplot(xlab = "Dias",
+                       ylab = "Infectados")+
+    ggtitle(label = "Evolución de los Casos de Infectados",
+            subtitle = "Desempeño del Modelo, Duarte")+
+    labs(caption = "Datos de los Boletines de Salud Pública")+
+    scale_color_manual( name = element_blank(), label = c("Datos", "Modelo"), values = c("blue", "red"))+
+    theme_bw()+
+    geom_point()
+  
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Santiago
+  beta_st <-  0.54
+  p_st = c (alpha0 = alpha0, beta0 = beta_st, kappa0 = kappa0, gamma = gamma_value, delta = delta_value)
+  W_st = 963422 -1  # suceptibles_0
+  X_st = 0           # Expuestos_0
+  Y_st = 1         # Infectados_0
+  Z_st = 0           # Recuperados_0
+  N_st = W_st + X_st + Y_st + Z_st #Poblacion
+  
+  valores_ini = c (S = W_st/N_st, E = X_st/N_st, I = Y_st/N_st, R = Z_st/N_st)
+  
+  #Solucionando el Sistema 
+  o_st = ode(valores_ini, tiempo_disc, modelo_seir, p_st)
+  o_st <- as.data.frame(o_st*N_st) 
+  
+  #Ajuste de los Casos Publicados por Salud Publica
+  s_st <- ts(o_st[1:28,3],freq = 365, start = c(2020,62))
+  d_st <- d[,26]
+  
+  data_st <- window(cbind(d_st,s_st),start=c(2020,80))
+  
+  data_st %>% autoplot(xlab = "Dias",
+                       ylab = "Infectados")+
+    ggtitle(label = "Evolución de los Casos de Infectados",
+            subtitle = "Desempeño del Modelo, Santiago")+
+    labs(caption = "Datos de los Boletines de Salud Pública")+
+    scale_color_manual( name = element_blank(), label = c("Datos", "Modelo"), values = c("blue", "red"))+
+    theme_bw()+
+    geom_point()
+  
+  
+  
